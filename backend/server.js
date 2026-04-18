31299169d2cd9815c16f3bc6d96b949edb4bdce6
Etsub82 <etsubdinkabreham63@gmail.com>
2026-04-18 14:34:41 +0300

    Implemented multiple patient display using reusable PatientCard component

--- backend/server.js
+++ backend/server.js
@@ -4,13 +4,21 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 
-app.get("/api/patient", (req, res) => {
-  res.json({
-    name: "Abel",
-    age: 28,
-    heartRate: 76,
-    temperature: 36.6
-  });
+app.get("/api/patients", (req, res) => {
+  res.json([
+    {
+      name: "Abel",
+      age: 28,
+      heartRate: 76,
+      temperature: 36.6
+    },
+    {
+      name: "Sara",
+      age: 25,
+      heartRate: 80,
+      temperature: 37.0
+    }
+  ]);
 });
 
 app.listen(5000, () => {
